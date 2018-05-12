172345276dc702ea52d198da002b4bbd6b195322
Dave Smith <davewsmith@gmail.com>
2018-05-12 09:02:54 -0700

    part V - add password

--- flask-tutorial/app/models.py
+++ flask-tutorial/app/models.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from app import db
 
 
@@ -10,6 +12,12 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
 
     notes = db.relationship('Note', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
