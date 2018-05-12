0f14cb26a7cd66a05d651e1bac3aaa77e55f07a8
Dave Smith <davewsmith@gmail.com>
2018-05-11 21:47:35 -0700

    part IV - migration for notes

--- flask-tutorial/app/models.py
+++ flask-tutorial/app/models.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from app import db
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -6,5 +9,18 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
+    notes = db.relationship('Note', backref='author', lazy='dynamic')
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    body = db.Column(db.String(140))
+
+    def __repr__(self):
+        return '<Note {}>'.format(self.body)
+
