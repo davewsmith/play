413ab1782e9382c1a813382aa32c96c29400c868
Dave Smith <davewsmith@gmail.com>
2018-05-11 20:28:00 -0700

    part ii

--- flask-tutorial/app/routes.py
+++ flask-tutorial/app/routes.py
@@ -1,6 +1,13 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    bindings = dict(
+        title='Home',
+        user=dict(
+            username='Dave',
+        ),
+    )
+    return render_template('index.html', **bindings)
