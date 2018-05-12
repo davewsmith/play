11b9432bddf5375bba6f05128730e6fb1f083af6
Dave Smith <davewsmith@gmail.com>
2018-05-12 11:05:49 -0700

    part V - add registration

--- flask-tutorial/app/routes.py
+++ flask-tutorial/app/routes.py
@@ -13,8 +13,14 @@ from flask_login import (
 )
 from werkzeug.urls import url_parse
 
-from app import app
-from app.forms import LoginForm
+from app import (
+    app,
+    db,
+)
+from app.forms import (
+    LoginForm,
+    RegistrationForm,
+)
 from app.models import (
     Note,
     User,
@@ -53,3 +59,18 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user! Please Sign In to continue.')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
