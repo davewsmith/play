52904da791b06f7c3a0fc2cbce34ee176103dfb1
Dave Smith <davewsmith@gmail.com>
2020-07-05 16:34:44 -0700

    Cleanups and doc

--- flask-worker-thread/demo.py
+++ flask-worker-thread/demo.py
@@ -1,12 +1,17 @@
 """
-Demonstrate a simple, in process work queue for Flask apps.
+Demonstrate a simple work queue for Flask apps that runs
+in-process, in a thread.
+
+This works for small, personal projects, but is not a sane
+approach for anything larger.
 """
 
-import queue
-import threading
+from queue import Queue
+from threading import Thread
 from time import sleep
 
 from flask import Flask, redirect, render_template
+
 
 app = Flask(__name__)
 app.config['WORK_TIME'] = 3  # seconds
@@ -24,23 +29,22 @@ def worker(queue):
         sleep(duration)  # simulate doing work
         print("did task {!r}".format(task))
 
-
 # Give the worker thread a Queue that's shared with the app, and start
-# the worker. The worker will block immediately until a route puts
-# a task into the queue.
-wq = queue.Queue()
-worker_thread = threading.Thread(target=worker, args=(wq,))
+# the worker. The worker will block immediately until a task is added
+# to the queue.
+work_queue = Queue()
+worker_thread = Thread(target=worker, args=(work_queue,))
 worker_thread.start()
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', size=wq.qsize())
+    return render_template('index.html', queue_size=work_queue.qsize())
 
 
-# Because this is a cheesy demo, a task is just a number.
-# Because we're only the develoment server, there's no issue
-# with muliple processes handing out identical numbers.
+# Because this is a cheesy demo, a task is just a number. And because
+# we're using the develoment server, there's no issue with muliple
+# processes handing out identical numbers.
 task = 0
 
 @app.route('/add_task')
@@ -48,5 +52,5 @@ def add_task():
     global task
     task += 1
     print("add task {!r}".format(task))
-    wq.put(task)
+    work_queue.put(task)
     return redirect('/')
