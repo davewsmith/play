f703dd721dedb7638366abe8c4e3f325e407acaf
Dave Smith <davewsmith@gmail.com>
2021-08-08 21:05:55 -0700

    animation limps along

--- csv2graph/test.js
+++ csv2graph/test.js
@@ -14,26 +14,56 @@ var graph = d3.select("#graph").append("svg")
                 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 var data = [
-    {"id": 1, "x": 100, "y": 100, "color": "green"},
-    {"id": 2, "x": 150, "y": 150, "color": "red"},
-    {"id": 3, "x": 200, "y": 200, "color": "purple"}
+    {"key": 1, "x": 100, "y": 100, "color": "green"},
+    {"key": 2, "x": 150, "y": 150, "color": "red"},
+    {"key": 3, "x": 200, "y": 200, "color": "purple"}
 ];
 
 function drawgraph() {
 
-    graph.selectAll(".circle")
-        .data(data, (d) => {d.id})
+    var circles = graph.selectAll(".circle")
+        .data(data, (d) => {return d.key.toString()});
+
+    console.log("update: " + circles.size());
+    console.log("exit  : " + circles.exit().size());
+
+    circles.exit().each((d) => {console.log("exiting key " + d.key)});
+
+    circles
+      .exit().transition()
+        .attr("r", 0)
+        .remove();
+
+    console.log("enter : " + circles.enter().size());
+    // console.log("to merge: " + circles.merge().size());
+
+    circles
       .enter().append("circle")
         .attr("class", "circle")
-        .attr("r", 10)
+        .attr("r", 0)
         .attr("cx", (d, i) => {return d.x})
         .attr("cy", (d, i) => {return d.y})
-        .attr("fill", (d, i) => {return d.color});
+        .attr("fill", (d, i) => {return d.color})
+      .transition()
+        .attr("r", 10)
+        .attr("fill", "gray");
+}
 
-        // .attr("class", "rect")
-        // .attr("x", (d, i) => {return d.y})
-        // .attr("y", (d, i) => {return d.y})
-        // .attr("height", 10)
-        // .attr("width", 10)
-        // .attr("fill", (d, i) => {return d.color});
+var interval;
+
+function animategraph() {
+    drawgraph();
+
+    interval = setInterval(() => {
+        // console.log("data pre: " + data[0].key + " to " + data[data.length -1].key);
+        data.shift();
+        data.push({
+            "key": data[data.length - 1].key + 1,
+            "x": d3.randomInt(graph_width)(),
+            "y": d3.randomInt(graph_height)(),
+            "color": "red"
+        });
+        // console.log("data post: " + data[0].key + " to " + data[data.length -1].key);
+        drawgraph();
+    }, 5 * 1000);
 }
