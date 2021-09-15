1cfd87c7ad46d2c3b1a79d8a2e20874c915fef3f
Dave Smith <davewsmith@gmail.com>
2021-09-14 19:39:07 -0700

    Call dot via Popen

--- flask-graphviz/app.py
+++ flask-graphviz/app.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,7 +8,16 @@ app = Flask(__name__)
 def home():
     with open('test.svg', 'r') as f:
         svg = f.read()
-    return render_template('index.html', svg=svg)
+    return render_template('index.html', svg=svg, msg='From file')
+
+@app.route('/popen')
+def with_popen():
+    cmd = ['dot', '-Tsvg']
+    input = 'digraph { a->b }'.encode()
+    proc = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    out, err = proc.communicate(input)
+    svg = out.decode('utf-8')
+    return render_template('index.html', svg=svg, msg='From Popen')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
