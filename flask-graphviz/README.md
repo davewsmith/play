3a48083d2413432a471fa50fba5d4b336f59fc68
Dave Smith <davewsmith@gmail.com>
2021-09-14 20:05:59 -0700

    Add a graphviz example

--- flask-graphviz/README.md
+++ flask-graphviz/README.md
@@ -1,4 +1,17 @@
 # Flask Graphviz
 
-Playing with driving [Graphviz](https://graphviz.org/) via a Flask app.
+Playing with using [Graphviz](https://graphviz.org/) in a Flask app.
+
+## To run (with VirtualBox and vagrant installed):
+
+    $ vagrant up
+    $ vagrant ssh
+    vagrant@ubuntu-bionic:~$ cd /vagrant
+    vagrant@ubuntu-bionic:/vagrant$ ./run
+
+Then fire up a browser outside of the VM and hit these URLs
+
+ * http://0.0.0.0:5000/ reads SVG from a file
+ * http://0.0.0.0:5000/popen invokes `dot` via subprocess.Popen
+ * http://0.0.0.0:5000/graphviz uses the [graphviz](https://graphviz.readthedocs.io/en/stable/) package
 
--- flask-graphviz/app.py
+++ flask-graphviz/app.py
@@ -1,23 +1,43 @@
 import subprocess
 
 from flask import Flask, render_template
+import graphviz
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    """
+    The simple case: Read from a file on disk
+    """
     with open('test.svg', 'r') as f:
         svg = f.read()
-    return render_template('index.html', svg=svg, msg='From file')
+    return render_template('index.html', svg=svg, msg='SVG from file')
 
 @app.route('/popen')
-def with_popen():
+def using_popen():
+    """
+    Invoke `dot` via subprocess.Popen, passing hand-constructed input
+    """
     cmd = ['dot', '-Tsvg']
     input = 'digraph { a->b }'.encode()
     proc = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     out, err = proc.communicate(input)
+    # error checking goes here
     svg = out.decode('utf-8')
-    return render_template('index.html', svg=svg, msg='From Popen')
+    return render_template('index.html', svg=svg, msg='Using suprocess.Popen')
+
+@app.route('/graphviz')
+def using_graphviz():
+    """
+    Use graphviz to construct the graph
+    """
+    dot = graphviz.Digraph(format='svg')
+    dot.node('a')
+    dot.node('b')
+    dot.edge('a', 'b')
+    svg = dot.pipe().decode('utf-8')
+    return render_template('index.html', svg=svg, msg='Using graphviz')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
