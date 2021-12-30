fa973fb98454f6e64a96fa67585fb9e7978bdde3
Dave Smith <davewsmith@gmail.com>
2021-12-29 19:54:43 -0800

    Add a preliminary stylesheet

--- flask-api-vue/app.py
+++ flask-api-vue/app.py
@@ -2,6 +2,12 @@ from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return send_from_directory('static', 'index.html')
+
+
+@app.route('/css/<path>')
+def serve_js(path):
+    return send_from_directory('static/css', path)
