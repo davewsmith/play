b10fa9daac70ca6380a05b782dc259dc5788f22b
Dave Smith <davewsmith@gmail.com>
2022-09-30 16:34:43 -0700

    Working proof-of-concept

---
+++ purpleairscraper/README.md
@@ -0,0 +1,29 @@
+# Scraping PurpleAir sensors
+
+A quick sketch of something I might use elsewhere.
+
+## Setup
+
+Copy `env.template` to `.env` and fill it in with PurpleAir API keys.
+
+    python3 -m venv venv
+    . venv/bin/activate
+    pip install -r requirements.txt
+
+Then, to scape data
+
+    venv/bin/python SENSOR_ID > data.json
+
+If the JSON returned from the PurpleAir API does not contain an 'error' key,
+the 'sensor' block is augmented with a few new keys.
+
+  * 'aqi' is derived per the AirNow table
+  * 'category' comes from the AirNow table
+  * 'category\_color' is from elsewhere in the AirNow document
+
+## References
+
+  * [PurpleAir API documentation](https://community.purpleair.com/t/making-api-calls-with-the-purpleair-api/180)
+  * [Technical Assistance Document for the
+Reporting of Daily Air Quality – the Air Quality
+Index (AQI)](https://www.airnow.gov/sites/default/files/2021-05/aqi-technical-assistance-document-sept2018.pdf)
---
+++ purpleairscraper/purpleair.py
@@ -0,0 +1,75 @@
+"""
+Pull data from a PurpleAir sensor and dump some interesting bits
+"""
+
+import json
+import os
+import sys
+
+import dotenv
+import requests
+
+
+dotenv.load_dotenv()
+
+
+def sensor_data(sensor_id):
+    headers = {
+        "X-API-Key": os.getenv('PURPLEAIR_READ_KEY'),
+    }
+    r = requests.get(
+        f'https://api.purpleair.com/v1/sensors/{sensor_id}',
+        headers=headers)
+    if r.status_code == 200:
+        return r.json()
+    # TODO; error handling
+    # print(json.dumps(r.json(), indent=2))
+    return {
+        "error": r.status_code,
+    }
+
+
+# https://www.airnow.gov/sites/default/files/2020-05/aqi-technical-assistance-document-sept2018.pdf
+BREAKPOINTS = [
+    [  0.0,  12.0,   0,  50, 'Good',           '#00e400'],  # noqa: E201
+    [ 12.1,  35.4,  51, 100, 'Moderate',       '#ffff00'],  # noqa: E201
+    [ 35.5,  55.4, 101, 150, 'Sensitive',      '#ff7e00'],  # noqa: E201
+    [ 55.5, 150.4, 151, 200, 'Unhealthy',      '#ff0000'],  # noqa: E201
+    [150.5, 250.4, 201, 300, 'Very Unhealthy', '#993f97'],
+    [250.5, 350.4, 301, 400, 'Hazardous',      '#7e0023'],
+    [350.5, 500.4, 501, 500, 'Hazardous',      '#7e0023'],
+]
+
+
+def pm25_to_aqi(pm25):
+    # returns a tuple of (AQI, category, RBG color)
+    assert pm25 >= 0.0
+    bp = [bp for bp in BREAKPOINTS if pm25 >= bp[0]][-1]
+    aqi = bp[2] + (pm25 - bp[0]) * (bp[3] - bp[2]) / (bp[1] - bp[0])
+    return (int(aqi), bp[4], bp[5])
+
+
+if __name__ == '__main__':
+    """
+    for i in range(252):
+        print(f'{i}: {pm25_to_aqi(float(i))}')
+    """
+
+    try:
+        sensor_id = int(sys.argv[1])
+    except Exception:
+        print(f"Usage: {sys.argv[0]} ID")
+        os.exit(1)
+
+    data = sensor_data(sensor_id)
+
+    if 'error' not in data:
+        sensor = data["sensor"]
+        temperature = sensor["temperature"]
+        aqi, category, color = pm25_to_aqi(sensor["pm2.5"])
+        sensor["aqi"] = aqi
+        sensor["category"] = category
+        # TODO: downgrade color if data is stale
+        sensor["category_color"] = color
+
+    print(json.dumps(data, indent=2))
