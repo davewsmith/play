833870ca5dc7b20adbb2ad022e491bf4c4d2ea68
Dave Smith <davewsmith@gmail.com>
2023-08-25 22:46:55 -0700

    Update to 20.04
    Also track down and resolve a wacky virtualbox issue, which
    requires building the virtual environment in /home/vagrant/
    (which isn't synchronized with the host) instead of /vagrant
    (which is). This manifested as a failure to upgrade pip

--- sqlalchemy/README.md
+++ sqlalchemy/README.md
@@ -3,13 +3,13 @@
 A bit of fluff, motivated by wanting to have something within reach to
 remind myself of how to do a few things.
 
-## TODO
-
-I reached for some old 18.04-based code to get started. Update it 20.04.
-
 ## Setup
 
+Assuming `vagrant` and `virtualbox` are installed,
+
     vagrant up
+
+builds an Ubuntu 20.04 VM
 
 ## Testing
 
--- sqlalchemy/provision.sh
+++ sqlalchemy/provision.sh
@@ -7,7 +7,13 @@ sudo apt-get upgrade
 sudo apt-get install -y build-essential python3-dev python3-setuptools python3-pip python3-venv
 sudo apt-get install -y sqlite3
 
-[ -d /vagrant ] && cd /vagrant
+cd /vagrant
 
-python3 -m venv venv
-venv/bin/pip install -r requirements.txt
+# N.B. We build the venv in /home/vagrant instead of /vagrant
+#  because of an issue with (not) deleting files sync'd folders.
+#  See https://github.com/hashicorp/vagrant/issues/12057 and
+#  https://www.virtualbox.org/ticket/8761
+python3 -m venv /home/vagrant/venv
+source /home/vagrant/venv/bin/activate
+pip install --upgrade pip wheel setuptools
+pip install -r requirements.txt
