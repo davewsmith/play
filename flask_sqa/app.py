2aa7b45d6a275d32a0c09ff3017b6bb622e3747e
Dave Smith <davewsmith@gmail.com>
2023-08-26 20:53:20 -0700

    disable foreign_key pragma

--- flask_sqa/app.py
+++ flask_sqa/app.py
@@ -12,17 +12,20 @@ app = Flask(__name__)
 app.config.from_object(TestConfig)
 db.init_app(app)
 
-if app.config['SQLALCHEMY_DATABASE_URI'].startswith('sqlite'): 
-    # Enable SQLite3 foreign key support so that deletes will cascade.
-    #
-    # The need for this is SQLite version dependent! See
-    # https://www.sqlite.org/pragma.html#pragma_foreign_keys
+if False:  # not needed for this example
+    if app.config['SQLALCHEMY_DATABASE_URI'].startswith('sqlite'): 
+        # Enable SQLite3 foreign key support so that deletes will cascade.
+        #
+        # This only impacts SQL executed against the connection.
+        #
+        # The need for this is SQLite version dependent! See
+        # https://www.sqlite.org/pragma.html#pragma_foreign_keys
 
-    from sqlalchemy import event
-    from sqlalchemy.engine import Engine
+        from sqlalchemy import event
+        from sqlalchemy.engine import Engine
 
-    @event.listens_for(Engine, "connect")
-    def _set_sqlite_pragma(dbapi_connection, connection_record):
-        cursor = dbapi_connection.cursor()
-        cursor.execute("PRAGMA foreign_keys=ON;")
-        cursor.close()
+        @event.listens_for(Engine, "connect")
+        def _set_sqlite_pragma(dbapi_connection, connection_record):
+            cursor = dbapi_connection.cursor()
+            cursor.execute("PRAGMA foreign_keys=ON;")
+            cursor.close()
