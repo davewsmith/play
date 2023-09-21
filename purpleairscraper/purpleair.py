2c952362faeb41969931f4524958d69aeb3b8413
Dave Smith <davewsmith@gmail.com>
2023-09-20 21:39:01 -0700

    Add 2021 EPA correction

--- purpleairscraper/purpleair.py
+++ purpleairscraper/purpleair.py
@@ -31,6 +31,42 @@ def sensor_data(sensor_id):
     }
 
 
+def correct(pm25, rh):
+    # EPA corrections for PurpleAir pm2.5 sensor values
+    # See references in the README
+
+    if False:
+        # 2019 US-wide correction
+        return 5.75 - (0.085 * rh) + (0.52 * pm25)
+
+    if True:
+        # 2021 correction for fire and smoke
+        # TODO: triple-check these numbers against the PDF
+        if pm25 < 30:
+            return 5.75 - (0.0862 * rh) + (0.524 * pm25)
+        elif pm25 < 50:
+            weight = (pm25 / 20) - (30 / 20)
+            return (
+                5.75 - (0.0862 * rh) +
+                ((0.786 * weight) + (0.524 * (1 - weight))) * pm25
+            )
+        elif pm25 < 210:
+            return 5.75 - (0.0862 * rh) + (0.786 * pm25)
+        elif pm25 < 260:
+            weight = (pm25 / 50) - (210 / 50)
+            return (
+                (0.0862 * rh) * (1 - weight) +
+                ((0.69 * weight) + (0.786 * (1 - weight))) * pm25 +
+                (2.966 * weight) + (5.75 * (1 - weight)) +
+                (8.84 * pow(10, -4)) * pow(pm25, 2) * weight
+            )
+        else:
+            return 2.966 + (0.69 * pm25) + (8.84 * pow(10, -4)) * pow(pm25, 2)
+
+    # uncorrected
+    return pm25
+
+
 # https://www.airnow.gov/sites/default/files/2020-05/aqi-technical-assistance-document-sept2018.pdf
 BREAKPOINTS = [
     [  0.0,  12.0,   0,  50, 'Good',           '#00e400'],  # noqa: E201
@@ -47,7 +83,7 @@ def pm25_to_aqi(pm25):
     # returns a tuple of (AQI, category, RBG color)
     assert pm25 >= 0.0
     bp = [bp for bp in BREAKPOINTS if pm25 >= bp[0]][-1]
-    aqi = bp[2] + (pm25 - bp[0]) * (bp[3] - bp[2]) / (bp[1] - bp[0])
+    aqi = bp[2] + (pm25 - bp[0]) * float(bp[3] - bp[2]) / float(bp[1] - bp[0])
     return (int(aqi), bp[4], bp[5])
 
 
@@ -60,7 +96,8 @@ def add_derived_aqi(sensor_data):
     """
     if 'error' not in sensor_data:
         sensor = sensor_data["sensor"]
-        aqi, category, color = pm25_to_aqi(sensor["pm2.5"])
+        corrected_pm25 = correct(sensor["pm2.5"], sensor["humidity"])
+        aqi, category, color = pm25_to_aqi(corrected_pm25)
         sensor["aqi"] = aqi
         sensor["category"] = category
         # TODO: downgrade color if data is stale
@@ -69,15 +106,16 @@ def add_derived_aqi(sensor_data):
 
 if __name__ == '__main__':
     """
-    for i in range(252):
-        print(f'{i}: {pm25_to_aqi(float(i))}')
+    humidity = 40
+    for pm25 in range(255):
+        print(f'{pm25}: {pm25_to_aqi(correct(float(pm25), 40))}')
     """
 
     try:
         sensor_id = int(sys.argv[1])
     except Exception:
         print(f"Usage: {sys.argv[0]} ID")
-        os.exit(1)
+        sys.exit(1)
 
     data = sensor_data(sensor_id)
     add_derived_aqi(data)
