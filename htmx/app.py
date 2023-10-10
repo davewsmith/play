8e35f23b6e0c46f9a5d1e5943b38d5c7e2acb553
Dave Smith <davewsmith@gmail.com>
2023-10-09 20:43:05 -0700

    Add a "hello world" example

--- htmx/app.py
+++ htmx/app.py
@@ -9,5 +9,15 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/example1')
+def example1():
+    return render_template('example1.html')
+
+
+@app.route('/clicked', methods=['POST'])
+def clicked():
+    return render_template('example1-clicked.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
