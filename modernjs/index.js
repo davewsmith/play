a593b9826dd311ca690e6374aef55fe571d8c36d
Dave Smith <davewsmith@gmail.com>
2024-04-07 20:15:19 -0700

    nuke moderjs

--- modernjs/index.js
+++
@@ -1,8 +0,0 @@
-// index.js
-import moment from 'moment';
-
-console.log("Hello from JavaScript!");
-console.log(moment().startOf('day').fromNow());
-
-var name = "Bob", time = "today";
-console.log(`Hello ${name}, how are you ${time}?`);
