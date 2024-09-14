fd9b4f11c3275aa9359e6e1bce1e5e823383bfe3
Dave Smith <davewsmith@gmail.com>
2024-09-13 21:12:09 -0700

    play with the example

--- fasthtml/docs_example.py
+++ fasthtml/docs_example.py
@@ -2,12 +2,25 @@
 
 from fasthtml.common import *
 
-app,rt = fast_app()
+app,rt = fast_app(
+    hdrs=[Style(':root { --pico-font-size: 120%; }')])
 
 @rt('/')
-def get(): return Div(P('Hello World!'), hx_get="/change")
+def get():
+    return (
+        Title('Demo'),
+        Main(
+            Div(
+                P('Hello World!'),
+                P('Good to see you'),
+                hx_get="/change",
+           ),
+           cls='container',
+        ),
+    )
 
 @rt('/change')
-def get(): return P('Nice to be here!')
+def get():
+    return P('Nice to be here!')
 
 serve()
